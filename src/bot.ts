7e758e8aefdd7ddbcdba10a3881d5e69db450728
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-11-04 12:26:47 -0300

    Quote wizard first approach

--- src/bot.ts
+++ src/bot.ts
@@ -4,12 +4,57 @@ import { Markup, Scenes, session, Telegraf } from 'telegraf'
 import 'dotenv/config'
 import { adduser, help, quote, info } from './libs'
 
+const quoteWizard = new Scenes.WizardScene(
+    'quote-wizard',
+    (ctx:any) => {
+      ctx.reply("Write the quote:");
+      ctx.wizard.state.data = {};
+      return ctx.wizard.next();
+    },
+    (ctx:any) => {
+      ctx.wizard.state.data.quote = ctx.message.text;
+      ctx.reply('Write the author of quote');
+      return ctx.wizard.next();
+    },
+    (ctx:any) => {
+      ctx.wizard.state.data.author = ctx.message.text;
+      ctx.reply(`Your quote: ${ctx.wizard.state.data.quote}
+  by: ${ctx.wizard.state.data.author}`
+      );
+      // Save quote.
+      return ctx.scene.leave();
+    }
+  );
+
+
+const stage = new Scenes.Stage([quoteWizard]);
+
+
 // Telegram Bot.
 const { BOT_TOKEN } = process.env
 if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!')
 
 // Telegraf constructor accepts a custom Context type
 const bot = new Telegraf(BOT_TOKEN);
+
+bot.use(session());
+bot.use(stage.middleware());
+
+// // Save all message on DynamoDB.
+// Middleware.
+// bot.use(async (context:any, next) => {
+//     await context.reply(JSON.stringify(context.update, null, 2))
+//     next()
+// })
+
+// TODO - Pasar a middelware.
+// bot.on(message("text"), context => {
+//     let user_id = context.from.id.toString()
+//     let message_id = context.message.message_id.toString()
+//     let date = context.message.date.toString()
+//     let text = context.message.text
+//     addMessage(message_id, user_id, date, text)
+// });
 
 
 // Commands.
@@ -33,48 +78,32 @@ bot.command('info', async (context: any) => {
 // add.
 bot.command('add', async (context: any) => {
     let userID = context.from.id.toString()
-    // context.scene.enter("question")
-    // let data = await
+    context.scene.enter('quote-wizard');
+    // // context.scene.enter("question")
+    // // let data = await
 
-    // console.log(data)
-    // let email;
-    // let password;
+    // // console.log(data)
+    // // let email;
+    // // let password;
 
-    // //Something like this
-    // context.reply("Enter your email");
-    // email = await context.message?.text;
+    // // //Something like this
+    // // context.reply("Enter your email");
+    // // email = await context.message?.text;
 
-    // context.reply("Enter your password");
-    // password = await context.message?.text;
+    // // context.reply("Enter your password");
+    // // password = await context.message?.text;
 
-    // console.log(email)
-    // console.log(password)
+    // // console.log(email)
+    // // console.log(password)
 
-    let quote = ''
-    let author = ''
+    // let quote = ''
+    // let author = ''
 
-    // await add(context, bot)
+    // // await add(context, bot)
 
-    // await addQuote(userID, quote, author)
-    context.reply('Your quote has saved')
+    // // await addQuote(userID, quote, author)
+    // context.reply('Your quote has saved')
 })
-
-
-// // Save all message on DynamoDB.
-// Middleware.
-// bot.use(async (context:any, next) => {
-//     await context.reply(JSON.stringify(context.update, null, 2))
-//     next()
-// })
-
-// TODO - Pasar a middelware.
-// bot.on(message("text"), context => {
-//     let user_id = context.from.id.toString()
-//     let message_id = context.message.message_id.toString()
-//     let date = context.message.date.toString()
-//     let text = context.message.text
-//     addMessage(message_id, user_id, date, text)
-// });
 
 
 // Set Telegram command menu.
@@ -97,6 +126,10 @@ bot.telegram.setMyCommands([
     }
 ])
 
+
+
+
+
 // Start function.
 bot.start((context:any) => {
     adduser(context)
@@ -106,23 +139,3 @@ bot.start((context:any) => {
 
 // Inicia el bot.
 bot.launch()
-
-// bot.action('quote', async (context: any) => {
-//     await context.reply( quote(context), { parse_mode: 'HTML' })
-// })
-
-
-// const inlineQuote = Markup.inlineKeyboard([
-//     Markup.button.callback('Quote', 'quote')
-// ]
-// )
-
-
-// const replyKeyboard = Markup.keyboard([
-//     ['Quote']
-// ]).resize()
-
-// bot.use(async (context:any, next) => {
-//     await context.reply('Quote', replyKeyboard)
-//     next()
-// })
